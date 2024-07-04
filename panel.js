0ea1a3db3651e8090d39c4a19b205e6377b69c3e
max <max@ma3x.com>
2024-07-04 23:19:34 +0200

    only once determining activeTabId

--- panel.js
+++ panel.js
@@ -1,64 +1,44 @@
-// Copyright 2023 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 const pButton = document.querySelector('#perplexity');
 
 pButton.addEventListener('click', () => {
 	// initiate listner
-	let data = '';
+	let activeTabId;
+	chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+		if (tabs && tabs.length > 0) {
+			activeTabId = tabs[0].id;
+		}
+	})
 	chrome.devtools.network.onRequestFinished.addListener(
 		function (request) {
 			if (request.request.url.startsWith('https://you.com/api/streamingSavedChat')) {
 				console.log("request streamingSavedChat", request);
-				request.getContent(function (content, encoding) {
+				request.getContent((content) => {
 					const events = content.split('\n\n');
 					for (const event of events) {
 						if (event.startsWith('event: youChatCachedChat')) {
 							const dataString = event.split('\n')[1].substring(5); // Remove 'data: ' prefix
 							try {
-								data = JSON.parse(dataString).chat_title;
+								const data = JSON.parse(dataString).chat_title;
 								// adding text to already existing element
-								document.getElementById('response').textContent = data;
-								/*
-								// creating a textarea that can easily be copied
-							const textArea = document.createElement('textarea');
-							textArea.value = data;
-							document.body.appendChild(textArea);
-							textArea.select();
-					*/
-								// active tab
-								chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-									if (tabs && tabs.length > 0) {
-										const activeTabId = tabs[0].id;
-										console.log('scripting');
-										chrome.scripting.executeScript({
-											target: { tabId: activeTabId },
-											function: (data) => {
-												console.log('activetab');
-												const copyButton = document.createElement('button');
-												copyButton.textContent = 'copy data to clipboard';
-												copyButton.style = 'top: 10px; position: absolute';
-												copyButton.addEventListener('click', () => {
-													console.log('copying');
-													navigator.clipboard.writeText(data);
-												},
-												);
-												document.body.appendChild(copyButton);
-											},
-											args: [data],
+								const responseContainer = document.getElementById('response');
+								const textArea = document.createElement('textarea');
+								textArea.value = data;
+								responseContainer.appendChild(textArea);
+								textArea.select();
+								chrome.scripting.executeScript({
+									target: { tabId: activeTabId },
+									function: (data) => {
+										console.log('activetab');
+										const copyButton = document.createElement('button');
+										copyButton.textContent = 'copy data to clipboard';
+										copyButton.style = 'top: 10px; position: absolute';
+										copyButton.addEventListener('click', () => {
+											console.log('copying');
+											navigator.clipboard.writeText(data);
 										});
-									}
+										document.body.appendChild(copyButton);
+									},
+									args: [data]
 								})
 							} catch (error) {
 								console.error('Error parsing JSON:', error);
@@ -69,13 +49,6 @@ pButton.addEventListener('click', () => {
 				});
 			}
 		}
-	);
-	// reload
-
-	chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-		if (tabs && tabs.length > 0) {
-			const activeTabId = tabs[0].id;
-			chrome.tabs.reload(activeTabId);
-		}
-	})
+	)
+	chrome.tabs.reload(activeTabId)
 });
